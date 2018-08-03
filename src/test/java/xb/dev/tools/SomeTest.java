8e501ceb27e7cad436c17e05ea5c3b023c1f616d
huangxb <18228033207@163.com>
2018-08-03 15:27:25 +0800

    简单测试类

---
+++ src/test/java/xb/dev/tools/SomeTest.java
@@ -0,0 +1,11 @@
+package xb.dev.tools;
+
+/**
+ * @Author: Created by huangxb on 2018-08-03 14:51:28
+ * @Description:
+ */
+public class SomeTest {
+    public static void main(String[] args){
+        System.out.println(!"1".equals("2"));
+    }
+}
