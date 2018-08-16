bdd9068038517902be0ee867e44cdbd4b74ae057
huangxb <18228033207@163.com>
2018-08-16 17:24:31 +0800

    添加web模块

---
+++ dev-tools-web/src/main/java/xb/dev/tools/web/controller/NewsController.java
@@ -0,0 +1,20 @@
+package xb.dev.tools.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @Author: Created by huangxb on 2018-08-16 15:46:01
+ * @Description:
+ */
+@Controller
+@RequestMapping("/news")
+public class NewsController {
+    @GetMapping("")
+    public ModelAndView index(){
+        return new ModelAndView("news_index");
+    }
+}
