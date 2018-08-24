12b778341ca45eb9281c999a4322a0a4e4cda180
huangxb <18228033207@163.com>
2018-08-24 18:47:49 +0800

    layui框架添加

--- dev-tools-web/src/main/java/xb/dev/tools/web/controller/NewsController.java
+++ dev-tools-web/src/main/java/xb/dev/tools/web/controller/NewsController.java
@@ -3,7 +3,6 @@ package xb.dev.tools.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -15,6 +14,6 @@ import org.springframework.web.servlet.ModelAndView;
 public class NewsController {
     @GetMapping("")
     public ModelAndView index(){
-        return new ModelAndView("news_index");
+        return new ModelAndView("layui");
     }
 }
