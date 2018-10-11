f8543823341e1e4121d57c080b203181e5a451fb
huangxb <18228033207@163.com>
2018-10-11 17:53:34 +0800

    网关添加

--- dev-basic-service/basic-service-gateway/src/main/java/xb/dev/basic/GatewayApplication.java
+++ dev-basic-service/basic-service-gateway/src/main/java/xb/dev/basic/GatewayApplication.java
@@ -1,8 +1,19 @@
 package xb.dev.basic;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 /**
  * @author Created by huangxb
  * @date 2018-09-30 11:13:03
  */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class GatewayApplication {
+    public static void main(String[] args){
+        SpringApplication.run(GatewayApplication.class);
+    }
 }
