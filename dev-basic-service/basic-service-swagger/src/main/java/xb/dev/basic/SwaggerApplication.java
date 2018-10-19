ada73a10592d76b9ef220a1ceae4be5d9150d773
huangxb <18228033207@163.com>
2018-10-19 09:51:17 +0800

    基础服务建设完成,统一注册到eureka,使用swagger服务拉取eureka上所有服务提供者接口文档

--- dev-basic-service/basic-service-swagger/src/main/java/xb/dev/basic/SwaggerApplication.java
+++ dev-basic-service/basic-service-swagger/src/main/java/xb/dev/basic/SwaggerApplication.java
@@ -11,7 +11,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
 public class SwaggerApplication {
 
     public static void main(String[] args) {
