629dec49386719f4d467e37e4c3f065cfd975078
huangxb <18228033207@163.com>
2018-11-26 17:35:42 +0800

    修改

---
+++ dev-tools-core/tools-core-es/src/main/java/xb/dev/tools/es/util/ListNode.java
@@ -0,0 +1,11 @@
+package xb.dev.tools.es.util;
+
+/**
+ * @author Created by huang xiao bao
+ * @date 2018-11-14 10:26:41
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+ }
