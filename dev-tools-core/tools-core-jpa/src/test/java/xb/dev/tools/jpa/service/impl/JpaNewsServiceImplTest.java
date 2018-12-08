a662a80c7c86a15169c3c7fa7c1d35e7a3160a05
huangxb <18228033207@163.com>
2018-12-08 11:36:40 +0800

    时间工具类添加

--- dev-tools-core/tools-core-jpa/src/test/java/xb/dev/tools/jpa/service/impl/JpaNewsServiceImplTest.java
+++ dev-tools-core/tools-core-jpa/src/test/java/xb/dev/tools/jpa/service/impl/JpaNewsServiceImplTest.java
@@ -10,7 +10,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 import xb.dev.tools.jpa.JpaApplication;
 import xb.dev.tools.jpa.dao.entity.JpaNewsEntity;
 import xb.dev.tools.jpa.dao.entity.QJpaNewsEntity;
+import xb.dev.tools.jpa.util.DateUtils;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -34,5 +40,13 @@ public class JpaNewsServiceImplTest {
                 .fetch();
         Map<Integer, Long> countMap = fetch.stream().collect(Collectors.toMap(key -> key.get(qJpaNewsEntity.userId), value -> value.get(qJpaNewsEntity.userId.count())));
         System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+//        calendar.add(Calendar.MONTH,1);
+        calendar.add(Calendar.MONTH,-1);
+        Date expire = DateUtils.getSpecifiedDayFromNow(DateUtils.TYPE_AFTER, 30);
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(calendar.getTime()));
     }
 }
